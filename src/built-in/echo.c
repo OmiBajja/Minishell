fe9dc6fc53d93b683fff8bb8a29424839b3fc99c
Omi Bajja <omarbajja99@gmail.com>
2025-06-28 16:08:17 +0200

    echo with -n

--- src/built-in/echo.c
+++ src/built-in/echo.c
@@ -6,7 +6,7 @@
 /*   By: obajja <obajja@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 00:47:17 by obajja            #+#    #+#             */
-/*   Updated: 2025/06/13 22:27:59 by obajja           ###   ########.fr       */
+/*   Updated: 2025/06/28 16:07:31 by obajja           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,27 @@
 
 void ft_echo(char **inputs)
 {
-	char *result;
+	char	*result;
+	int		i;
+	int		n;
+	int		j;
 
-	result = ft_strscomp(inputs);
-	printf("%s\n",result);
+	i = -1;
+	n = 0;
+	while (inputs[++i])
+	{
+		j = 3;
+		if (ft_strncmp(inputs[i], "-n", 2) == 0)
+			while (inputs[i][j] == 'n')
+				j++;
+		if (inputs[i][j])
+			break ;
+		n = 1;
+	}
+	result = ft_strscomp(&inputs[i]);
+	if (n == 0 && i == 0)
+		printf("%s\n", result);
+	else
+		printf("%s", result);
 	free(result);
 }
