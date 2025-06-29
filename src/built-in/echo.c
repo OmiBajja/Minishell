906c355f765d1b6220fd554d9c845a51656fa92c
Omi Bajja <omarbajja99@gmail.com>
2025-06-29 14:27:36 +0200

    echo -n finally fixed and working, off by one with shlvl, and deleted print tokens for clarity

--- src/built-in/echo.c
+++ src/built-in/echo.c
@@ -3,36 +3,81 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pafranci <pafranci@student.42.fr>          +#+  +:+       +#+        */
+/*   By: obajja <obajja@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 00:47:17 by obajja            #+#    #+#             */
-/*   Updated: 2025/06/28 16:07:31 by obajja           ###   ########.fr       */
+/*   Updated: 2025/06/29 14:24:45 by obajja           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+int	where_print(char **inputs)
+{
+	int	i;
+	int j;
+
+	i = 0;
+	j = -1;
+	if ((inputs[1][0] && inputs[1][0] != '-')
+		|| (inputs[1][1] && inputs[1][1] != 'n'))
+		return (1);
+	while (inputs[++i])
+	{
+		if (!inputs[i][1])
+			return(i);
+		if ((inputs[i][0] && inputs[i][0] != '-')
+		|| (inputs[i][1] && inputs[i][1] != 'n'))
+			return (i);
+		j = 1;
+		while (inputs[i][++j])
+		{
+			printf("input:%c\n",inputs[i][j]);
+			if (inputs[i][j] != 'n')
+				return (i);
+		}
+	}
+	return (i);
+}
+
+int	is_newline(char **inputs)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if (!inputs[i][1])
+		return(0);
+	if ((inputs[i][0] && inputs[i][0] != '-')
+		|| (inputs[i][1] && inputs[i][1] != 'n'))
+		return (0);
+	else
+	{
+		j = 1;
+		while (inputs[i][++j])
+		{
+			if (inputs[i][j] != 'n')
+				return (0);
+		}
+		return (1);
+	}
+
+
+}
+
 void	ft_echo(char **inputs)
 {
 	char	*result;
+	int		nl;
 	int		i;
-	int		n;
-	int		j;
 
-	i = -1;
-	n = 0;
-	while (inputs[++i])
-	{
-		j = 3;
-		if (ft_strncmp(inputs[i], "-n", 2) == 0)
-			while (inputs[i][j] == 'n')
-				j++;
-		if (inputs[i][j])
-			break ;
-		n = 1;
-	}
+	i = 0;
+	nl = is_newline(inputs);
+	if (nl == 1)
+		i = where_print(inputs);
 	result = ft_strscomp(&inputs[i]);
-	if (n == 0 && i == 0)
+	if (nl == 0)
 		printf("%s\n", result);
 	else
 		printf("%s", result);
