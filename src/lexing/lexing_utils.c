b186e77a0df44b7bfd376aed88e0f6153c01dadf
Omi Bajja <omarbajja99@gmail.com>
2025-07-03 17:53:39 +0200

    lexing fixed to accept ec"ho" x as the command echo x

---
+++ src/lexing/lexing_utils.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexing_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obajja <obajja@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 17:49:14 by obajja            #+#    #+#             */
+/*   Updated: 2025/07/03 17:52:03 by obajja           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+char	*unquote_handler(char *input, int *i, t_mini *mini)
+{
+	int		start;
+	char	*result;
+	char	*temp;
+
+	start = *i;
+	while (input[*i] && !ft_is_whitespace(input[*i])
+		&& input[*i] != '\'' && input[*i] != '"'
+		&& !operator_check(&input[*i]))
+		(*i)++;
+	result = ft_strndup(&input[start], *i - start);
+	if (!result)
+		return (NULL);
+	if (is_extendable(result) != -1)
+	{
+		temp = ft_extender(result, mini->env, mini);
+		if (!temp)
+			return (free (result), NULL);
+		free (result);
+		result = temp;
+	}
+	return (result);
+}
+
+char	*word_handler(char *input, int *i, t_mini *mini)
+{
+	if (input[*i] == '\'' || input[*i] == '"')
+		return (quote_handler(input, i, mini->env));
+	else
+		return (unquote_handler(input, i, mini));
+}
