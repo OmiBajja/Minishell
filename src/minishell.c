851fba0f8b7604315a69e6448e769a83e99bf55d
Omi Bajja <omarbajja99@gmail.com>
2025-07-09 10:29:00 +0200

    input changed back the way it was

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: obajja <obajja@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 12:02:04 by obajja            #+#    #+#             */
-/*   Updated: 2025/07/04 14:33:08 by obajja           ###   ########.fr       */
+/*   Updated: 2025/07/09 10:27:55 by obajja           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ int	mini_handler(t_mini *mini)
 
 	while (42)
 	{
-		input = readline("bash-5.2$ ");
+		input = readline(BRED "MinisHell :" RESET_COLOR);
 		if (!input)
 		{
 			mini_cleaner(mini);
