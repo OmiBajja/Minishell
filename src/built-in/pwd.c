872e75197526a319d2c63757a32ddb650ccbc303
Omi Bajja <omarbajja99@gmail.com>
2025-07-15 16:27:40 +0200

    print in dev/full, shllvl more robust

--- src/built-in/pwd.c
+++ src/built-in/pwd.c
@@ -6,7 +6,7 @@
 /*   By: obajja <obajja@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 00:53:56 by obajja            #+#    #+#             */
-/*   Updated: 2025/07/12 00:41:01 by obajja           ###   ########.fr       */
+/*   Updated: 2025/07/15 14:43:39 by obajja           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,27 @@ int	ft_pwd(void)
 {
 	char	cwd[1024];
 	char	*pwd;
+	int		err;
 
 	pwd = getcwd(cwd, sizeof(cwd));
 	if (pwd)
-		ft_printf_fd(1, "%s\n", pwd);
+	{
+		err = write(STDOUT_FILENO, pwd, ft_strlen(pwd));
+		if (err != -1)
+			err = write(STDOUT_FILENO, "\n", 1);
+	}
 	else
 	{
-		ft_printf_fd(2, "pwd: error retrieving current directory: getcwd: can");
-		ft_printf_fd(2, "not parent directories: No such file or directory\n");
+		ft_printf_fd(2, "pwd: error retrieving current directory:");
+		ft_printf_fd(2, "getcwd: cannot parent directories:");
+		ft_printf_fd(2, " No such file or directory\n");
+		return (1);
+	}
+	if (err == -1)
+	{
+		ft_printf_fd(2, "Minishell: pwd: write");
+		ft_printf_fd(2, " error: No space left on device\n");
+		return (1);
 	}
 	return (0);
 }
--- src/utils/ft_shllvl.c
+++ src/utils/ft_shllvl.c
@@ -6,20 +6,73 @@
 /*   By: obajja <obajja@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 14:53:05 by obajja            #+#    #+#             */
-/*   Updated: 2025/06/29 14:59:31 by obajja           ###   ########.fr       */
+/*   Updated: 2025/07/15 16:20:33 by obajja           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+static	void	shl_not_set(t_mini *mini, int i, int err)
+{
+	char	**env_temp;
+
+	if (err == 1)
+	{
+		if (mini->env[i])
+			free(mini->env[i]);
+		mini->env[i] = ft_strdup("SHLVL=0");
+		if (!mini->env[i])
+			return (ft_freestrs(mini->env));
+	}
+	else
+	{
+		env_temp = ft_strsjoin(mini->env, "SHLVL=0");
+		if (!env_temp)
+			return ;
+		free(mini->env);
+		mini->env = env_temp;
+	}
+}
+
+static	void	true_shlvl(t_mini *mini, int i)
+{
+	int		shllvl_num;
+	char	*shllvl_string;
+	char	*result;
+
+	shllvl_num = ft_atoi(&mini->env[i][6]);
+	if (shllvl_num < 999)
+		shllvl_num += 1;
+	shllvl_string = ft_itoa(shllvl_num);
+	result = ft_strjoin("SHLVL=", shllvl_string);
+	free(shllvl_string);
+	free(mini->env[i]);
+	mini->env[i] = result;
+}
+
 void	ft_shllvl(t_mini *mini)
 {
 	int	j;
+	int	i;
+	int	err;
 
-	j = -1;
-	while (mini->env[++j])
+	i = -1;
+	j = 5;
+	err = 0;
+	while (mini->env[++i] && err == 0)
 	{
-		if (ft_strncmp(mini->env[j], "SHLVL=", ft_strlen("SHLVL=")) == 0)
-			mini->env[j][6] += 1;
+		if (ft_strncmp(mini->env[i], "SHLVL=", ft_strlen("SHLVL=")) == 0)
+		{
+			while (mini->env[i][++j] && err == 0)
+			{
+				if (!ft_isdigit(mini->env[i][j]))
+					err = 1;
+			}
+			if (err != 1)
+				true_shlvl(mini, i);
+			break ;
+		}
 	}
+	if (!mini->env[i] || err == 1)
+		shl_not_set(mini, i, err);
 }
